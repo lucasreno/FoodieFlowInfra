fdfd8660b9fc204a2117ec92883129270c2e8864
Lucas Renó <lucasreno9@gmail.com>
2024-05-14 20:28:20 +0100

    fix: resources database

--- terraform/postgres.tf
+++ terraform/postgres.tf
@@ -14,14 +14,14 @@ resource "google_sql_user" "postgres" {
   password = "postgres"
 }
 
-resource "google_sql_database" "postgres" {
+resource "google_sql_database" "db-order" {
   name     = "order"
   instance = google_sql_database_instance.postgres.name
   charset  = "UTF8"
   collation = "en_US.UTF8"
 }
 
-resource "google_sql_database" "postgres" {
+resource "google_sql_database" "db-pay" {
   name     = "pay"
   instance = google_sql_database_instance.postgres.name
   charset  = "UTF8"
